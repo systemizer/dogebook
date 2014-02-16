e70135a0f25874557d2dbda61a063cce08ece569
Rob McQueen <rob@systemizer.me>
2014-02-16 11:52:38 -0800

    so garbage. many collection

--- dogebook/dogebook_web/static/js/main.js
+++ dogebook/dogebook_web/static/js/main.js
@@ -1,17 +1,25 @@
 $(document).ready(function() {
 
-	var doge_phrases = ['such drift', 'much dignity', 'very leeder', 'many robe', 'much infinity', 'so gif', 'very guitar', 'such fall', 'so trick', 'much feels', 'many happy', 'very art', 'amaze', 'wow', 'excite', 'much joanna', 'such rob'];
-	var doge_colors = ['#6FFF00','#FF00FF', '#FFFF00','#4D4DFF','#FE0001','#FF4105','#993CF3'];
+    var doge_phrases = ['such drift', 'much dignity', 'very leeder',
+			'many robe', 'much infinity', 'so gif',
+			'very guitar', 'such fall', 'so trick', 'much feels',
+			'many happy', 'very art', 'amaze', 'wow', 'excite',
+			'much joanna', 'such rob'],
+    doge_colors = ['#6FFF00','#FF00FF', '#FFFF00','#4D4DFF',
+		   '#FE0001','#FF4105','#993CF3'],
+    body$ = $('body');
+    var elems = [];
 
-	$(window).scroll(function() {
-		// $('.doge-phrase').remove();
-		var doge_phrase = doge_phrases[Math.floor(Math.random() * 17)];
-		var doge_color = doge_colors[Math.floor(Math.random() * 7)];
-		var top_percentage = Math.floor(Math.random()*100) + 1;
-		var left_percentage = Math.floor(Math.random()*100) + 1;
-		var font_size = Math.random()*1.5;
-		$('body').append('<div class="doge-phrase" style="top: '+top_percentage+'%; left: '+left_percentage+'%; color: '+doge_color+'; font-size:'+font_size+'em;">'+doge_phrase+'</div>');
-	});
-
-
+    $(window).scroll(function() {
+	// $('.doge-phrase').remove();
+	var doge_phrase = doge_phrases[Math.floor(Math.random() * 17)];
+	var doge_color = doge_colors[Math.floor(Math.random() * 7)];
+	var top_percentage = Math.floor(Math.random()*100) + 1;
+	var left_percentage = Math.floor(Math.random()*100) + 1;
+	var font_size = Math.random()*1.5;
+	var elem$ = $('<div class="doge-phrase" style="top: '+top_percentage+'%; left: '+left_percentage+'%; color: '+doge_color+'; font-size:'+font_size+'em;">'+doge_phrase+'</div>');
+	body$.append(elem$);
+	elems.push(elem$);
+	setTimeout(function() {elems[0].remove();elems = elems.splice(1,elems.length)},Math.random()*10000);
+    });
 });
