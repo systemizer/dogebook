0631c7bf47b3b7a59708bedb93e9662b7177e6bc
Rob McQueen <rob@systemizer.me>
2014-02-16 13:33:15 -0800

    too dogify

--- dogebook/dogebook_web/templatetags/dogify.py
+++ dogebook/dogebook_web/templatetags/dogify.py
@@ -9,7 +9,9 @@ DEFAULT_DOGE_PHRASES = ['such drift', 'much dignity', 'very leeder',
 			'many happy', 'very art', 'amaze', 'wow', 'excite',
 			'to the moon!']
 
+COMMON_WORDS = ["just","and","enough","hella","wicked","great","of","a","only","that"]
 DOGE_OPERATORS = ["so","much","such","many","very","how","too"]
+TO_REPLACE_WORDS = COMMON_WORDS + DOGE_OPERATORS
 
 @register.filter
 def dogify_comment(value):
@@ -19,7 +21,7 @@ def dogify_comment(value):
     if len(words) == 1:
         words = [random.choice(DOGE_OPERATORS),words[0]]
     else:
-        words = map(lambda x: random.choice(DOGE_OPERATORS) if x.lower() in DOGE_OPERATORS else x,
+        words = map(lambda x: random.choice(DOGE_OPERATORS) if x.lower() in TO_REPLACE_WORDS else x,
                     words)
 
     return " ".join(words)
