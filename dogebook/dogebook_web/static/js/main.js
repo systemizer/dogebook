ac91f53b475735c29ca0d86201a382fb425071bd
Joanna Kao <joannaskao@gmail.com>
2014-02-17 03:58:25 -0500

    http => https. much ssl. wow.

--- dogebook/dogebook_web/static/js/main.js
+++ dogebook/dogebook_web/static/js/main.js
@@ -61,7 +61,7 @@ $(document).ready(function() {
 		/* Usage: $el.click( function(e) { AJMINT.sendTweet(e) }) */
 		// This function only needs e but if you want to pass in special text or a url hash, you can
 		AJMINT.sendTweet = function(e, text, route){
-			var base_url = 'https://twitter.com/intent/tweet?url=' + ((!route) ? window.location.href : ('http://' + window.location.hostname + window.location.pathname + route));
+			var base_url = 'https://twitter.com/intent/tweet?url=' + ((!route) ? window.location.href : ('https://' + window.location.hostname + window.location.pathname + route));
 			text = (text) ? text : 'very photo, much dogebombed';
 
 			var tweet_text  = "&text=" + text,
@@ -80,16 +80,16 @@ $(document).ready(function() {
 			AJMINT.sendFbShare = function(e, text, route){
 			var base_url = 'http://www.facebook.com/dialog/feed',
 			    app_id   = '?app_id=1405417853046999',
-			    page_url = '&link=' + ((!route) ? window.location.href : ('http://' + window.location.hostname + window.location.pathname + route));
+			    page_url = '&link=' + ((!route) ? window.location.href : ('https://' + window.location.hostname + window.location.pathname + route));
 
 			var name = "&name=dogebomb: much bomb, very doge",
 			    description = "&description=much bomb, very doge",
-			    redirect    = '&redirect_uri=http://dogebomb.me',
-			    image       = '&picture=' + ((!route) ? window.location.href : ('http://' + window.location.hostname + window.location.pathname + route));;
+			    redirect    = '&redirect_uri=https://dogebomb.me',
+			    image       = '&picture=' + ((!route) ? window.location.href : ('https://' + window.location.hostname + window.location.pathname + route));;
 
 			var facebook_url = base_url + app_id + page_url + name + description + redirect + image;
 			    facebook_url = AJMINT.percentEncode(facebook_url);
-			// console.log(facebook_url)
+			console.log(facebook_url)
 
 			var settings = 'width=900,height=450,scrollbars=no,location=0,statusbars=0,menubars=0,toolbars=0,resizable=0';
 
@@ -98,7 +98,7 @@ $(document).ready(function() {
 
 		AJMINT.sendGplusShare = function(e, route){
 			var base_url = 'https://plus.google.com/share',
-			    page_url = '?url=' + ((!route) ? window.location.href : ('http://' + window.location.hostname + window.location.pathname + route));
+			    page_url = '?url=' + ((!route) ? window.location.href : ('https://' + window.location.hostname + window.location.pathname + route));
 
 			var gplus_url = base_url + page_url;
 			    gplus_url = AJMINT.percentEncode(gplus_url);
