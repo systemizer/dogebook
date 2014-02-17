cb9342252a4e229552951d1139b57287c6cb53a1
Rob McQueen <rob@systemizer.me>
2014-02-16 20:43:20 -0800

    much diverse. no unique

--- dogebook/dogebook_web/models.py
+++ dogebook/dogebook_web/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class DogeImage(models.Model):
-    fb_id = models.CharField(max_length=127,unique=True)
+    fb_id = models.CharField(max_length=127)
     owner = models.ForeignKey(User)
     image = models.ImageField(upload_to="dogeimages",blank=True,null=True)
     comment = models.TextField(null=True,blank=True)
