1579282bc2b2000cf9151ae4f4172ced486aeb03
Rob McQueen <rob@systemizer.me>
2014-02-16 20:47:54 -0800

    much fix. much fix

--- dogebook/dogebook_web/utils.py
+++ dogebook/dogebook_web/utils.py
@@ -20,43 +20,46 @@ def dogify(fb_photos,user):
     returned_images = []
 
     for photo in fb_photos:
-        if photo['id'] in known_images_map:
-            returned_images.append(known_images_map[photo['id']])
+        try:
+            if photo['id'] in known_images_map:
+                returned_images.append(known_images_map[photo['id']])
+                continue
+
+
+            #Rotate DOge
+            doge_img_copy = doge_img.copy()
+            doge_img_copy = doge_img_copy.rotate(int(random.random()*45-22.5))
+
+            #Flip Doge
+            if random.random() > .75:
+                doge_img_copy = doge_img_copy.transpose(Image.FLIP_LEFT_RIGHT)
+
+            url = photo['picture'].replace("s.jpg","b.jpg")
+
+            file = cStringIO.StringIO(urllib.urlopen(url).read())
+            background = Image.open(file)
+            doge_img_w,doge_img_h=doge_img_copy.size
+            bg_w,bg_h=background.size
+            offset=(int((bg_w-doge_img_w)/8 + (bg_w-doge_img_w)/4*3*random.random()),int((bg_h-doge_img_h)/8 + (bg_h-doge_img_h)/4*3*random.random()))
+            background.paste(doge_img_copy,offset,doge_img_copy)
+
+            imagefile = cStringIO.StringIO()
+            background.save(imagefile,format="PNG")
+
+            comment = None
+            if photo.get("comments"):
+                comment = photo.get("comments")['data'][0]['message']
+
+            doge_image = DogeImage(fb_id=photo['id'],owner=user,comment=comment)
+            doge_image.save()
+            doge_image.image.save(
+                os.path.basename(url),
+                ContentFile(imagefile.getvalue()),
+                save=True
+            )
+            known_images_map[doge_image.fb_id] = doge_image
+            returned_images.append(doge_image)
+        except Exception:
             continue
-
-
-        #Rotate DOge
-        doge_img_copy = doge_img.copy()
-        doge_img_copy = doge_img_copy.rotate(int(random.random()*45-22.5))
-
-        #Flip Doge
-        if random.random() > .75:
-            doge_img_copy = doge_img_copy.transpose(Image.FLIP_LEFT_RIGHT)
-
-        url = photo['picture'].replace("s.jpg","b.jpg")
-
-        file = cStringIO.StringIO(urllib.urlopen(url).read())
-        background = Image.open(file)
-        doge_img_w,doge_img_h=doge_img_copy.size
-        bg_w,bg_h=background.size
-        offset=(int((bg_w-doge_img_w)/8 + (bg_w-doge_img_w)/4*3*random.random()),int((bg_h-doge_img_h)/8 + (bg_h-doge_img_h)/4*3*random.random()))
-        background.paste(doge_img_copy,offset,doge_img_copy)
-
-        imagefile = cStringIO.StringIO()
-        background.save(imagefile,format="PNG")
-
-        comment = None
-        if photo.get("comments"):
-            comment = photo.get("comments")['data'][0]['message']
-
-        doge_image = DogeImage(fb_id=photo['id'],owner=user,comment=comment)
-        doge_image.save()
-        doge_image.image.save(
-            os.path.basename(url),
-            ContentFile(imagefile.getvalue()),
-            save=True
-        )
-        known_images_map[doge_image.fb_id] = doge_image
-        returned_images.append(doge_image)
 
     return returned_images
